bd34c7a046a08f6b5c7c65f565bda85b4da6547b
helyezerteofilo <helyezerteofilo2@hotmail.com>
2023-11-05 17:13:00 -0300

    feat: more endpoints added and improved

--- src/services/service.js
+++ src/services/service.js
@@ -1,33 +1,57 @@
 const supabase = require("./client");
 
+function averageCalculator(restaurantes) {
+  const restaurantesComMedia = restaurantes.map((restaurante) => {
+    if (restaurante.Avaliacao.length > 0) {
+      const notas = restaurante.Avaliacao.map((avaliacao) => avaliacao.nota);
+      const media = notas.reduce((total, nota) => total + nota, 0) / notas.length;
+      return { ...restaurante, mediaNotas: media };
+    } else {
+      return { ...restaurante, mediaNotas: null };
+    }
+  });
+  return restaurantesComMedia;
+};
+
 const eatsyService = {
 
-    async getAllRestaurants() {
-        let { data: Restaurante, error } = await supabase
-        .from('Restaurante')
-        .select('*')
+  async getAllRestaurants() {
+    let { data: Restaurante, error } = await supabase
+      .from('Restaurante')
+      .select('*, Avaliacao(nota),Foto(imagem)')
 
-        if (error) {
-          console.error("Erro ao buscar dados do Supabase:", error);
-          return null;
-        }
-        console.log(Restaurante)
-        return Restaurante;
-      },
+    if (error) {
+      console.error("Erro ao buscar dados do Supabase:", error);
+      return null;
+    }
+    return averageCalculator(Restaurante);
+  },
 
-    async getAllUsers() {
-        let { data: Usuario, error } = await supabase
-        .from('Usuario')
-        .select('*')
+  async getAllUsers() {
+    let { data: Usuario, error } = await supabase
+      .from('Usuario')
+      .select('*')
 
-        if (error) {
-            console.error("Erro ao buscar dados do Supabase:", error);
-            return null;
-        }
-        console.log(Usuario)
-        return Usuario;
-        },
-      
+    if (error) {
+      console.error("Erro ao buscar dados do Supabase:", error);
+      return null;
+    }
+    console.log(Usuario)
+    return Usuario;
+  },
+
+  async getRestaurantById(id) {
+    let { data: Restaurante, error } = await supabase
+      .from('Restaurante')
+      .select('*, Avaliacao(comentario,nota,Usuario(*)),Foto(imagem)')
+      .eq('id_restaurante', id)
+
+    if (error) {
+      console.error("Erro ao buscar dados do Supabase:", error);
+      return null;
+    }
+    return averageCalculator(Restaurante);
+  },
 
 }
 
--- src/services/test.js
+++ src/services/test.js
@@ -1,32 +1,44 @@
 const eatsyService = require("./service");
 
 async function testGetAllRestaurants() {
-    try {
-      const restaurants = await eatsyService.getAllRestaurants();
-      if (restaurants) {
-        console.log("Restaurantes:", restaurants);
-      } else {
-        console.log("Erro ao buscar restaurantes.");
-      }
-    } catch (error) {
-      console.error("Erro inesperado:", error);
+  try {
+    const restaurants = await eatsyService.getAllRestaurants();
+    if (restaurants) {
+      console.log("Restaurantes:", restaurants[3]);
+    } else {
+      console.log("Erro ao buscar restaurantes.");
     }
+  } catch (error) {
+    console.error("Erro inesperado:", error);
   }
+}
 
 async function testGetAllUsers() {
-    try {
-      const users = await eatsyService.getAllUsers();
-      if (users) {
-        console.log("Usuarios:", users);
-      } else {
-        console.log("Erro ao buscar restaurantes.");
-      }
-    } catch (error) {
-      console.error("Erro inesperado:", error);
+  try {
+    const users = await eatsyService.getAllUsers();
+    if (users) {
+      console.log("Usuarios:", users);
+    } else {
+      console.log("Erro ao buscar restaurantes.");
     }
+  } catch (error) {
+    console.error("Erro inesperado:", error);
   }
+}
 
+async function testGetRestaurantById() {
+  try {
+    const restaurant = await eatsyService.getRestaurantById(4);
+    if (restaurant) {
+      console.log("Restaurante:", restaurant[0]);
+    } else {
+      console.log("Erro ao buscar restaurantes.");
+    }
+  } catch (error) {
+    console.error("Erro inesperado:", error);
+  }
+}
 
+testGetRestaurantById();
 testGetAllRestaurants();
-testGetAllUsers()
-  
+testGetAllUsers();
