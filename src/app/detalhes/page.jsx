26d3ce41f186928bf287d1e60c2f184593916179
carloxo307 <carlos.aleixo@rede.ulbra.br>
2023-11-15 16:41:00 -0300

    feat: adiciona tela de detalhes do restaurante

---
+++ src/app/detalhes/page.jsx
@@ -0,0 +1,38 @@
+export default function Detalhes() {
+
+    return (
+        <>
+            <div className="container p-5">
+                <div className="d-flex justify-content-center container">
+                    <div className="row cols-12 ">
+                        <div className="col">
+                            <div className="ratio ratio-21x9">
+                                <img src="/images/imagemRestaurante.png" className=""></img>
+                            </div>
+                            <div className="nomeRestaurante pt-5">
+                                <p href="#" className="">Nome do Restaurante</p>
+                            </div>
+                            <div className="container p-0">
+                                <div className="row">
+                                    <div className="col">
+                                        <div className="infoRestaurante pb-2">
+                                            <p>aaaaaaaaaaa</p>
+                                        </div>
+                                    </div>
+                                    <div className="col d-flex justify-content-end">
+                                        <p>bbbbbbbbbb</p>
+                                    </div>
+
+                                </div>
+
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </>
+    )
+}
