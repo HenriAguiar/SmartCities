5474be87ede03d14bd70da4697ac3d46e8992a1b
carloxo307 <carlos.aleixo@rede.ulbra.br>
2023-11-18 18:36:04 -0300

    feat: faixa na home

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -42,10 +42,21 @@ export default function Home() {
           <div className="row row-cols-1 row-cols-md-2 row-cols-lg-5 gy-4 justify-center items-center">
             {recommendedRestaurants.map((restaurante) => (
               <div key={restaurante.id_restaurante} className="">
-                <CardRestaurante imgUrl={restaurante.Foto[0].imagem} avaliacao={restaurante.mediaNotas} nome={restaurante.nome} id={restaurante.id_restaurante}/>
+                <CardRestaurante imgUrl={restaurante.Foto[0].imagem} avaliacao={restaurante.mediaNotas} nome={restaurante.nome} id={restaurante.id_restaurante} />
               </div>
             ))}
           </div>
+        </div>
+      </div>
+      <div className="container">
+        <div className="faixa bg-green-200 text-white text-center py-8">
+          <h2 className="text-2xl font-bold mb-4">Quem Somos</h2>
+          <p className="text-lg">
+            Buscamos trazer um cardápio com as melhores opções para uma boa refeição na nossa cidade
+          </p>
+          <p className="text-lg">
+            Nosso objetivo é conectar os amantes da boa comida com os melhores restaurantes da região!
+          </p>
         </div>
       </div>
       <div>
@@ -56,7 +67,7 @@ export default function Home() {
           <div className="row justify-center items-center">
             {topRatedRestaurants.map((restaurante) => (
               <div key={restaurante.id_restaurante} className="col-12 col-md-6 col-lg-3 mb-4">
-                <CardRestaurante imgUrl={restaurante.Foto[0].imagem} avaliacao={restaurante.mediaNotas} nome={restaurante.nome} id={restaurante.id_restaurante}/>
+                <CardRestaurante imgUrl={restaurante.Foto[0].imagem} avaliacao={restaurante.mediaNotas} nome={restaurante.nome} id={restaurante.id_restaurante} />
               </div>
             ))}
           </div>
